d3c2ce004a72d217c057504d1444a31d5ae63a9b
Gwendal-github <60733867+Gwendal-github@users.noreply.github.com>
2021-03-16 19:29:50 +0100

    Début debug Flatlist

--- ListSpacecrafts.tsx
+++ ListSpacecrafts.tsx
@@ -1,46 +1,69 @@
 import React from 'react';
-import { StyleSheet, Text, StatusBar, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, StatusBar, FlatList, TouchableOpacity, View } from 'react-native';
 import { Table, Row } from 'react-native-table-component';
 import { Card,Modal } from 'react-native-paper';
 import {useQuery } from 'react-query'
 import fetchAsync from './lib/fetch'
 
+
+const ModalCard = (item) => {
+  const [visible, setVisible] = React.useState(false);
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  return (
+    <TouchableOpacity onPress={showModal}>
+      <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={{backgroundColor: 'white', padding: 20}}>
+        <Text>{item.name}</Text>
+      </Modal>
+    </TouchableOpacity>
+  );
+}
+
 const renderItem=({item}) => {
-    const [visible, setVisible] = React.useState(false);
-    const showModal = () => setVisible(true);
-    const hideModal = () => setVisible(false);
-    const containerStyle = {backgroundColor: 'white', padding: 20};
     const dataTable = {
         tableHead: ['Crew Size','Cargo Capacity'],
         tableData: [item.crew,item.cargo_capacity]
     }
-    return(
-        <TouchableOpacity onPress={showModal}>
-                <Card style={styles.card}>
-                <Text style={styles.title}>{item.name}</Text>
-                <Text style={styles.subtitle}>{item.model}</Text>
-                <Table style={styles.table}>
-                    <Row data={dataTable.tableHead} style={styles.row}/>
-                    <Row data={dataTable.tableData} style={styles.row}/>
-                </Table>
-                <Text style={styles.subtitle}>Price : {item.cost_in_credits}</Text>   
-                </Card>
-                <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
-                    <Text>{item.name}</Text>
-                </Modal>
-        </TouchableOpacity>
-    );
+    console.log(item.nom); 
+    /*return(
+      <View>
+        <Card style={styles.card}>
+        <Text style={styles.title}>{item.name}</Text>
+        <Text style={styles.subtitle}>{item.model}</Text>
+        <Table style={styles.table}>
+            <Row data={dataTable.tableHead} style={styles.row}/>
+            <Row data={dataTable.tableData} style={styles.row}/>
+        </Table>
+        <Text style={styles.subtitle}>Price : {item.cost_in_credits}</Text>   
+        </Card>
+      </View>
+    );*/
+    return(<Text>Ca marche</Text>)
 };
 
 const ListSpacecrafts = () => {
   
-  const {isLoading, isError,data, error} = useQuery('data',fetchAsync);
-  console.log(isLoading);
-  console.log(data);
+  const {isLoading, isError,data, error} = useQuery('data',() => fetchAsync);
+  if(isLoading) {
+    console.log("Loading...");
+  }
+  if(isError){
+    console.log("error : "+error);
+  }
+  console.log("START : "+data+" FIN");
   return (
-    <FlatList data={data.results} renderItem={renderItem} />
-  );
+    <View>
+      <FeedSpaceCraft data={data} />
+    </View> 
+  )
+  
 };
+
+const FeedSpaceCraft = (data) => {
+  return (
+    <FlatList data={data.results} renderItem={renderItem} /> 
+  );
+}
 
 const styles = StyleSheet.create({
   safeContainer: {
